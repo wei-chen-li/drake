02d1710c69e89a0586609324145a979e67aa17cf
Wei-Chen Li <williamrob104@gmail.com>
2025-03-02 13:04:32 +0800

    Add unscented Kalman filter

--- systems/estimators/nonlinear_kalman_filter_internal.h
+++ systems/estimators/nonlinear_kalman_filter_internal.h
@@ -49,6 +49,93 @@ void ExtractLowerTriMatrix(
     Eigen::Ref<Eigen::MatrixXd> lower_tri_matrix);
 
 /**
+ * Computes the unscented transform sigma points X and weights wₘ, Wc. Returns
+ * a tuple (X, wₘ, Wc). X is a matrix where each column is a sigma point. wₘ is
+ * a vector containing the mean weights, allowing us to compute the mean using
+ * X * wₘ. Wc is a matrix, allowing us to compute the covariance using X * Wc *
+ * X.transpose().
+ */
+template <typename ParamsType>
+std::tuple<Eigen::MatrixXd, Eigen::VectorXd, Eigen::MatrixXd>
+UnscentedTransform(const Eigen::Ref<const Eigen::VectorXd>& mean,
+                   const Eigen::Ref<const Eigen::MatrixXd>& covariance,
+                   const ParamsType& params) {
+  const int n = mean.size();
+  const double alpha = params.alpha;
+  const double beta = params.beta;
+  const double kappa = (std::get_if<0>(&params.kappa))
+                           ? std::get<0>(params.kappa)
+                           : std::get<1>(params.kappa)(n);
+  const double lambda = alpha * alpha * (n + kappa) - n;
+  const Eigen::MatrixXd L = ((n + lambda) * covariance).llt().matrixL();
+
+  // Compute sigma points.
+  const int num_points = 2 * n + 1;
+  Eigen::MatrixXd points(mean.size(), num_points);
+  points.col(0) = mean;
+  for (int i = 0; i < n; ++i) {
+    points.col(i + 1) = mean + L.col(i);
+    points.col(i + 1 + n) = mean - L.col(i);
+  }
+
+  // Compute weights for mean.
+  Eigen::VectorXd w_m = 0.5 / (n + lambda) * Eigen::VectorXd::Ones(num_points);
+  w_m(0) = lambda / (n + lambda);
+
+  // Compute weights for covariance.
+  Eigen::VectorXd w_c = w_m;
+  w_c(0) += 1 - alpha * alpha + beta;
+
+  Eigen::MatrixXd I_minus_wm =
+      Eigen::MatrixXd::Identity(num_points, num_points).colwise() - w_m;
+  Eigen::MatrixXd W_c = I_minus_wm * w_c.asDiagonal() * I_minus_wm.transpose();
+
+  return {points, w_m, W_c};
+}
+
+/**
+ * A variant of the function UnscentedTransform() that takes a std::pair for
+ * mean and covariance.
+ */
+template <typename ParamsType>
+std::tuple<Eigen::MatrixXd, Eigen::VectorXd, Eigen::MatrixXd>
+UnscentedTransform(
+    const std::pair<Eigen::VectorXd, Eigen::MatrixXd>& mean_and_covariance,
+    const ParamsType& params) {
+  return UnscentedTransform(mean_and_covariance.first,
+                            mean_and_covariance.second, params);
+}
+
+/**
+ * Calculates the joint mean and covariance given multiple independently
+ * distributed Gaussians specified by their mean and covariance.
+ */
+inline std::pair<Eigen::VectorXd, Eigen::MatrixXd> JointGaussian() {
+  // Base case for JointGaussian() recursion.
+  return {Eigen::VectorXd(0), Eigen::MatrixXd(0, 0)};
+}
+template <typename... Args>
+std::pair<Eigen::VectorXd, Eigen::MatrixXd> JointGaussian(
+    const Eigen::Ref<const Eigen::VectorXd>& mean,
+    const Eigen::Ref<const Eigen::MatrixXd>& covariance, const Args&... args) {
+  DRAKE_ASSERT(covariance.rows() == mean.size() &&
+               covariance.cols() == mean.size());
+
+  auto [sub_mean, sub_covariance] = JointGaussian(args...);
+
+  int new_size = mean.size() + sub_mean.size();
+  Eigen::VectorXd joint_mean(new_size);
+  joint_mean << mean, sub_mean;
+
+  Eigen::MatrixXd joint_covariance = Eigen::MatrixXd::Zero(new_size, new_size);
+  joint_covariance.block(0, 0, mean.size(), mean.size()) = covariance;
+  joint_covariance.block(mean.size(), mean.size(), sub_mean.size(),
+                         sub_mean.size()) = sub_covariance;
+
+  return {joint_mean, joint_covariance};
+}
+
+/**
  * Returns a lower triangular matrix L, where LL' = M₁M₁' + M₂M₂'.
  */
 Eigen::MatrixXd MatrixHypot(const Eigen::Ref<const Eigen::MatrixXd>& M1,
