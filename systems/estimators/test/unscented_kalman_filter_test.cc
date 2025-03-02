6d5b9a6e58b60a6283d0d6254076895c95316fe8
Wei-Chen Li <williamrob104@gmail.com>
2025-03-02 13:04:32 +0800

    Add unscented Kalman filter

---
+++ systems/estimators/test/unscented_kalman_filter_test.cc
@@ -0,0 +1,99 @@
+#include "drake/systems/estimators/unscented_kalman_filter.h"
+
+#include <gtest/gtest.h>
+
+#include "drake/systems/estimators/test/nonlinear_kalman_filter_test.h"
+
+namespace drake {
+namespace systems {
+namespace estimators {
+namespace {
+class DiscreteTimeUKFTest : public DiscreteTimeNonlinearKalmanFilterTest<
+                                UnscentedKalmanFilterOptions> {
+ private:
+  std::unique_ptr<GaussianStateObserver<double>> MakeObserver(
+      const System<double>& observed_system,
+      const Eigen::Ref<const Eigen::MatrixXd>& W,
+      const Eigen::Ref<const Eigen::MatrixXd>& V,
+      const UnscentedKalmanFilterOptions& options) const override {
+    return UnscentedKalmanFilter(observed_system,
+                                 *observed_system.CreateDefaultContext(), W, V,
+                                 options);
+  }
+};
+
+TEST_F(DiscreteTimeUKFTest, Contruction) {
+  this->TestConstruction(false);
+}
+TEST_F(DiscreteTimeUKFTest, ErrorDynamics) {
+  this->TestErrorDynamics(false);
+}
+TEST_F(DiscreteTimeUKFTest, SteadyState) {
+  this->TestSteadyState(false);
+}
+TEST_F(DiscreteTimeUKFTest, ExplicitNoiseErrorDynamics) {
+  this->TestExplicitNoiseErrorDynamics(false);
+}
+
+class ContinuousDiscreteUKFTest
+    : public ContinuousDiscreteNonlinearKalmanFilterTest<
+          UnscentedKalmanFilterOptions> {
+ private:
+  std::unique_ptr<GaussianStateObserver<double>> MakeObserver(
+      const System<double>& observed_system,
+      const Eigen::Ref<const Eigen::MatrixXd>& W,
+      const Eigen::Ref<const Eigen::MatrixXd>& V,
+      double discrete_measurement_time_period,
+      const UnscentedKalmanFilterOptions& options_temp) const override {
+    auto options = options_temp;
+    options.discrete_measurement_time_period = discrete_measurement_time_period;
+    return UnscentedKalmanFilter(observed_system,
+                                 *observed_system.CreateDefaultContext(), W, V,
+                                 options);
+  }
+};
+
+TEST_F(ContinuousDiscreteUKFTest, Contruction) {
+  this->TestConstruction(false);
+}
+TEST_F(ContinuousDiscreteUKFTest, ErrorDynamics) {
+  this->TestErrorDynamics(false);
+}
+TEST_F(ContinuousDiscreteUKFTest, Simulation) {
+  this->TestSimulation(false);
+}
+TEST_F(ContinuousDiscreteUKFTest, ExplicitNoiseErrorDynamic) {
+  this->TestExplicitNoiseErrorDynamics(false);
+}
+
+class ContinuousTimeUKFTest : public ContinuousTimeNonlinearKalmanFilterTest<
+                                  UnscentedKalmanFilterOptions> {
+ private:
+  std::unique_ptr<GaussianStateObserver<double>> MakeObserver(
+      const System<double>& observed_system,
+      const Eigen::Ref<const Eigen::MatrixXd>& W,
+      const Eigen::Ref<const Eigen::MatrixXd>& V,
+      const UnscentedKalmanFilterOptions& options) const override {
+    return UnscentedKalmanFilter(observed_system,
+                                 *observed_system.CreateDefaultContext(), W, V,
+                                 options);
+  }
+};
+
+TEST_F(ContinuousTimeUKFTest, Contruction) {
+  this->TestConstruction(false);
+}
+TEST_F(ContinuousTimeUKFTest, ErrorDynamics) {
+  this->TestErrorDynamics(false);
+}
+TEST_F(ContinuousTimeUKFTest, SteadyState) {
+  this->TestSteadyState(false);
+}
+TEST_F(ContinuousTimeUKFTest, ExplicitNoiseErrorDynamics) {
+  this->TestExplicitNoiseErrorDynamics(false);
+}
+
+}  // namespace
+}  // namespace estimators
+}  // namespace systems
+}  // namespace drake
