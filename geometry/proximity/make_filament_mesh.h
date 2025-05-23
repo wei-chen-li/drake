841f546336a45a9b9af81978ec1d002630a8f47d
Wei-Chen Li <williamrob104@gmail.com>
2025-05-23 16:39:25 +0800

    Implement MakeFilamentSurfaceMesh()

--- geometry/proximity/make_filament_mesh.h
+++ geometry/proximity/make_filament_mesh.h
@@ -10,15 +10,12 @@ namespace internal {
 /* Creates a surface mesh for the given `filament`; the level of
  tessellation is guided by the `resolution_hint`.
 
- @param filament         The filament for which a surface mesh is created.
- @param resolution_hint  The size of the mesh in guaranteed to be smaller than
-                         resolution_hint.
+ @param filament The filament for which a surface mesh is created.
  @return The triangulated surface mesh for the given filament.
- @tparam T double or float.
+ @tparam_double_only
 */
 template <typename T>
-TriangleSurfaceMesh<T> MakeFilamentSurfaceMesh(const Filament& filament,
-                                               double resolution_hint);
+TriangleSurfaceMesh<T> MakeFilamentSurfaceMesh(const Filament& filament);
 
 }  // namespace internal
 }  // namespace geometry
